289bee8f397e36740b657b5ea402e1c146779133
Eric Mott <ericmott10@gmail.com>
2018-02-24 23:29:40 -0800

    Created a word list to randomly pick a word for the user to find. Also implemented a points system, the user gets 10 points if the picture taken was correct.

--- projects/project 3/source/GoogleApiExample/GoogleApiExample/MainActivity.cs
+++ projects/project 3/source/GoogleApiExample/GoogleApiExample/MainActivity.cs
@@ -6,18 +6,19 @@ using System.Collections.Generic;
 using Android.Content.PM;
 using Android.Provider;
 using Android.Support.V7.App;
+using System;
 
 namespace GoogleApiExample
 {
     [Activity(Label = "iFindIt", Icon = "@mipmap/icon")]
     public class MainActivity : Activity
     {
-        string label1 = null;
-        string label2 = null;
-        string label3 = null;
+        int total_points = 0;
+        int count = 0;
         Android.Graphics.Bitmap bitmap;
         ImageView imageView;
         private Vibrator myVib;
+        string GivenWord;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -33,10 +34,12 @@ namespace GoogleApiExample
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
-            if (IsThereAnAppToTakePictures() == true)
+            ImageButton startGame = FindViewById<ImageButton>(Resource.Id.start_game);
+
+            startGame.Click += delegate
             {
-                FindViewById<ImageButton>(Resource.Id.launchCameraButton).Click += TakePicture;
-            }
+                startFindIt();
+            };
         }
 
         //If the back button is pressed go back to the main layout to start over
@@ -59,6 +62,28 @@ namespace GoogleApiExample
             return availableActivities != null && availableActivities.Count > 0;
         }
 
+        private void startFindIt()
+        {
+            SetContentView(Resource.Layout.GoFind);
+            TextView whatToFind = (TextView)FindViewById(Resource.Id.whatToFind);
+
+
+            String[] WordList = { "technology", "computer keyboard", "basketball" , "circle", "car", "umbrella"};
+
+            Random rnd = new Random();
+            int rand = rnd.Next(0,3);
+
+            whatToFind.Text = (WordList[rand]);
+            GivenWord = WordList[rand];
+
+            if (IsThereAnAppToTakePictures() == true)
+            {
+                FindViewById<Button>(Resource.Id.go).Click += TakePicture;
+            }
+           
+
+        }
+
 
         private void TakePicture(object sender, System.EventArgs e)
         {
@@ -70,8 +95,14 @@ namespace GoogleApiExample
         // Called automatically whenever an activity finishes
         protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
         {
+            
+            int rank = -1;
+            float score = 0;
             base.OnActivityResult(requestCode, resultCode, data);
             SetContentView(Resource.Layout.Results);
+
+            TextView result = (TextView)FindViewById(Resource.Id.result);
+            TextView percentage = (TextView)FindViewById(Resource.Id.percentage);
 
             //Test to make sure user took a picture
             if (data != null)
@@ -89,6 +120,7 @@ namespace GoogleApiExample
 
             else
             {
+                Toast.MakeText(this, "Error: Starting over", ToastLength.Short).Show();
                 StartMainLayout();
             }
 
@@ -144,17 +176,37 @@ namespace GoogleApiExample
                 //ExecuteAsync instead
                 var apiResult = client.Images.Annotate(batch).Execute();
 
-                //Take the label result and through it into a string
-                label1 = apiResult.Responses[0].LabelAnnotations[0].Description;
-                label2 = apiResult.Responses[0].LabelAnnotations[1].Description;
-                label3 = apiResult.Responses[0].LabelAnnotations[2].Description;
-                FindViewById<TextView>(Resource.Id.labelResult1).Text = label1;
-                FindViewById<TextView>(Resource.Id.labelResult2).Text = label2;
-                FindViewById<TextView>(Resource.Id.labelResult3).Text = label3;
+                count = apiResult.Responses[0].LabelAnnotations.Count;
 
-                //string pic_result1 = apiResult.Responses[0].LabelAnnotations[0].Description;
-                //string pic_result2 = apiResult.Responses[1].LabelAnnotations[1].Description;
-                //string pic_result3 = apiResult.Responses[2].LabelAnnotations[2].Description;
+                for (int i = 0; i < count; i++)
+                {
+                    if (GivenWord == apiResult.Responses[0].LabelAnnotations[i].Description)
+                    {
+                        rank = i;
+                    }
+                }
+
+                score = (float)(apiResult.Responses[0].LabelAnnotations[rank].Score);
+                score *= 100;
+
+
+                if (rank != -1)
+                {
+                    result.Text = ("Correct!! +10 Points");
+                    percentage.Text = ("Your picture was " + score + "% accurate!" );
+                    total_points += 10;
+
+                    if (total_points >= 100)
+                    {
+                        //implement
+
+                    }
+                }
+
+                else
+                {
+                    result.Text = ("LOSER!!");
+                }
 
                 imageView.SetImageBitmap(bitmap);
                 bitmap = null;
